143d067ef8700918e0869f471907940973a84333
tnoor99 <tnoor049@gmail.com>
2024-03-18 03:32:23 -0400

    add changes to include e-sign option

--- documents.php
+++ documents.php
@@ -22,6 +22,8 @@
 <head>
     <?php require_once('universal.inc') ?>
     <title>Empowerhouse VMS | Documents</title>
+    <!-- Adding code to line below for adding jquery library for better event handling like submit action -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
@@ -37,6 +39,18 @@
                             <div class="row">
                                 <ul id="DocumentsListTabsList"
                                     style="margin: 0 0 20px;padding: 0;float: left;border-bottom: 1px solid #e5e5e5;width: 100%;">
+                                    
+                                    <!-- document links added below -->
+                                    <li class="column">
+                                    <a href="#" class="document-link" data-document-id="1" data-document-name="Criminal Background Check">Criminal Background Check</a>
+                                    </li>
+
+                                    <li class="column">
+                                    <a href="#" class="document-link" data-document-id="2" data-document-name="Acknowledgement of Receipt">Acknowledgement of Receipt</a>
+                                    </li>
+
+                                    <!-- my edits done -->
+
                                     <div class="column">
                                         <li id="DocumentsListFilesTab"
                                             style="position: relative;display: block;list-style-type: none;background: 0 0;float: left;margin: 0 4px 0 0;padding: 0;font-weight: 700;height: 35px;font-size: 14px;">
@@ -94,4 +108,34 @@
     </main>
 </body>
 
+<!-- JavaScript to handle signature submission -->
+<script>
+
+    $(document).ready(function () {
+            // add signature forms dynamically based on document links
+            $('.document-link').click(function (e) {
+                e.preventDefault(); // to prevent default link behavior 
+                var documentId = $(this).data('document-id');
+                var documentName = $(this).data('document-name');
+                var formHtml = '<form class="document-sign-form" data-document-id="' + documentId + '">';
+                formHtml += '<label for="signature-' + documentId + '">Your E-Signature for ' + documentName + ':</label>';
+                formHtml += '<input type="text" id="signature-' + documentId + '" name="signature" required>';
+                formHtml += '<button type="button" class="sign-button">Sign</button>';
+                formHtml += '</form>';
+                $('#signature-forms-container').html(formHtml); // Replace existing form with new form
+            });
+
+            // Handle signature submission
+            $(document).on('click', '.sign-button', function() {
+                var form = $(this).closest('.signature-form');
+                var documentId = form.data('document-id');
+                var signature = form.find('input[name="signature"]').val();
+                // Here you can handle the signature data, such as sending it to the server
+                alert('Document ID: ' + documentId + '\nSignature: ' + signature);
+                // Clear the signature input after submission (optional)
+                form.find('input[name="signature"]').val('');
+            });
+        });
+    </script>
+</body>
 </html>
