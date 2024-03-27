fddbf7de74aa8ac99f55f81b1fae22ad6eee68a8
1DianaGuzman1 <dguzman2@mail.umw.edu>
2024-03-27 19:03:52 -0400

    Username and Password Change

--- database/dbinfo.php
+++ database/dbinfo.php
@@ -21,8 +21,8 @@ function connect() {
     $user = "housedbdup";
     $pass = "housedbdup";
     if ($_SERVER['SERVER_NAME'] == 'jenniferp119.sg-host.com') {
-        $user = 'uwpcgsjb3tzec';
-        $database = 'dbyrqpvdjpzamq';
+        $user = 'housedbdup';
+        $database = 'housedbdup';
     }
     $con = mysqli_connect($host,$user,$pass,$database);
     if (!$con) { echo "not connected to server"; return mysqli_error($con);}
