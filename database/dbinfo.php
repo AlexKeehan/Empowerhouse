7914a545cca79d4d310aaa6245fb03773842315e
tnoor99 <tnoor049@gmail.com>
2024-04-09 03:53:28 -0400

    updating my end of dbinfo

--- database/dbinfo.php
+++ database/dbinfo.php
@@ -17,6 +17,9 @@
 
 function connect() {
     $host = "localhost"; 
+    $database = "duplicateHousedb";
+    $user = "duplicateHousedb";
+    $pass = "duplicateHousedb";
     if ($_SERVER['SERVER_NAME'] == 'jenniferp119.sg-host.com') {
         $user = 'uwpcgsjb3tzec';
         $database = 'dbyrqpvdjpzamq';
